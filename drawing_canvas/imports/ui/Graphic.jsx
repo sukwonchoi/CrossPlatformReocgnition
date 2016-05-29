3a7442d0c5dc8c1a7793d5077f89b58bbddeb2f3
Sukwon Choi <kswc95@gmail.com>
2016-05-29 15:26:31 +1200

    added score into alert

--- drawing_canvas/imports/ui/Graphic.jsx
+++ drawing_canvas/imports/ui/Graphic.jsx
@@ -48,7 +48,7 @@ export default class Graphic extends Component{
   clearCanvas(){
     console.log(this.clickX + " " + this.clickY);
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    alert(window.pdollar.Recognize(pointArray).Name);
+    alert(window.pdollar.Recognize(pointArray).Name + " Score: " + window.pdollar.Recognize(pointArray).Score);
     this.clickX.length = 0;
     this.clickY.length = 0;
     this.pointArray.length = 0;
