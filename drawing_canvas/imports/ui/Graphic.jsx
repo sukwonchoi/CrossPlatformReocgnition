448d88b4d6791885ebb2ebabdc0cd472fc372ff0
Jay Kim <jkim506@aucklanduni.ac.nz>
2016-06-01 14:31:20 +1200

    output change

--- drawing_canvas/imports/ui/Graphic.jsx
+++ drawing_canvas/imports/ui/Graphic.jsx
@@ -56,7 +56,7 @@ export default class Graphic extends Component{
   clearCanvas(){
     console.log(this.clickX + " " + this.clickY);
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    alert(window.pdollar.Recognize(pointArray).Name + " Score: " + window.pdollar.Recognize(pointArray).Score);
+    alert("Gesture: " + window.pdollar.Recognize(pointArray).Name + " Score: " + window.pdollar.Recognize(pointArray).Score);
     this.clickX.length = 0;
     this.clickY.length = 0;
     this.pointArray.length = 0;
