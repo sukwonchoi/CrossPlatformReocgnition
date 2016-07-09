7ab30778a168efa93c0e92b089f836332d72fc01
Jay Kim <jkim506@aucklanduni.ac.nz>
2016-07-09 13:18:05 +1200

    Minor refactor

---
+++ drawing_canvas/client/components/RecognitionCanvas.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import { Point, PDollarRecognizer } from './PDollar.js'
+
+
+import InkStore from '../stores/InkStore.js';
+
+export default class Graphic extends Component{
+
+}
